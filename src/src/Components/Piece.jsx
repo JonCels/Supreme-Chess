dadd2f00fa52c6cb6e306b139cdb0c0a6cbbbbb3
Rupinder Nagra <rupinder0410@gmail.com>
2021-03-20 17:57:34 -0400

    adding drag and position property to piece

--- src/src/Components/Piece.jsx
+++ src/src/Components/Piece.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
-import { useDrag } from 'react-dnd';
+import { useDrag } from 'react-dnd'
 
-export default function Piece({ piece: { type, color } }) {
-    const pieceImg = `./assets/${type}_${color}.png`
-    const [{isDragging}, drag] = useDrag({
-        item: {type: type},
-        collect: monitor => ({
-            isDragging: !!monitor.isDragging()
-        }),
+export default function Piece({
+    piece: { type, color },
+    position,
+}) {
+    const [{ isDragging }, drag, preview] = useDrag({
+        item: {
+            type: 'piece',
+            id: `${position}_${type}_${color}`,
+        },
+        collect: (monitor) => {
+            return { isDragging: !!monitor.isDragging() }
+        },
     })
+    const pieceImg = `./assets/${type}_${color}.png`
     return (
-        <div className="piece-container">
-            <img src={pieceImg} className="piece" ref={drag} style={{opacity: isDragging ? 0.5 : 1}}/>
-        </div>
+        <>
+            <div
+                className="piece-container"
+                ref={drag}
+                style={{ opacity: isDragging ? 0 : 1 }}
+            >
+                <img src={pieceImg} alt="" className="piece" />
+            </div>
+        </>
     )
 }
